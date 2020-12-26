7c39ee4a1a8a7b13e26a02b8ce74489f4c187c97
unknown <mposadaagudelosn@gmail.com>
2020-12-26 02:13:26 -0500

    changes

--- Web/js/script.js
+++ Web/js/script.js
@@ -18,3 +18,14 @@ const closeDialog = () => {
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
 });
+
+//Detectar números
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
