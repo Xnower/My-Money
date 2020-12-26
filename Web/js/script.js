e2fc007df018a2279e3ace6c3241ba163217e332
unknown <mposadaagudelosn@gmail.com>
2020-12-26 15:47:38 -0500

    changes

--- Web/js/script.js
+++ Web/js/script.js
@@ -21,3 +21,19 @@ window.addEventListener("click", function(event) {// When the user clicks anywhe
     modal.style.display = "none";
   }
 })
+
+function myFunction(ModalID) {
+  
+
+}
+
+//Only allow numbers and decimals
+function isNumberKey(evt)
+       {
+          var charCode = (evt.which) ? evt.which : evt.keyCode;
+          if (charCode != 46 && charCode > 31 
+            && (charCode < 48 || charCode > 57))
+             return false;
+
+          return true;
+       }
