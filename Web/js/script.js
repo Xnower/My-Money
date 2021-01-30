466f467daf6ed84ace47ad807fc9b27b77858643
unknown <mposadaagudelosn@gmail.com>
2021-01-30 00:58:46 -0500

    Changes

--- Web/js/script.js
+++ Web/js/script.js
@@ -4,12 +4,12 @@ var gB = 0; // Get gastos básicos
 var aM = 0; // Get ahorros mensuales
 var gP = 0; // Get gastos personales
 var tG = 0; // Get total gastos
+var points = 0; // Get total points
 
 // Porcentajes
 var gBP = 0; // Get gastos básicos porcentaje
 var aMP = 0; // Get ahorros mensuales porcentaje
 var gPP = 0; // Get gastos personales porcentaje
-
 
 /*
 window.addEventListener("click", function(event) {// When the user clicks anywhere outside of the modal, close it
@@ -103,6 +103,7 @@ console.log(gBP + " " + aMP + " " + gPP)
 
   if (gB <= gBP) {
     document.getElementById("result_1").innerHTML = "- Tus gastos básicos están por debajo del 50% de tus ingresos. Tienes un buen balance en relación con tus gastos básicos y tus ingresos mensuales.";
+    points = points + 1
   }
 
   if (aM < aMP) {
@@ -111,7 +112,8 @@ console.log(gBP + " " + aMP + " " + gPP)
 
   if (aM >= aMP) {
     document.getElementById("result_2").innerHTML = "- Tus ahorros superan el 20% de tus ingresos. Asegurate de encontrar dónde invertir parte de ellos, para generar una mayor rentabilidad.";
-    }
+    points = points + 1 
+  }
 
   if (gP > gPP) {
     document.getElementById("result_3").innerHTML = "- Tus gastos personales superan el 30% de tus ingresos, debes moderar moderada tus gastos personales, comer más seguido en casa y reducir tus compras puede ayudarte.";
@@ -119,6 +121,22 @@ console.log(gBP + " " + aMP + " " + gPP)
 
   if (gP <= gPP) {
     document.getElementById("result_3").innerHTML = "- Tus gastos personales están por debajo del 30% de tus ingresos. Recuerda encontrar un balance en tu vida social, salir de vez en cuando no está mal, pero exceder puede causarte problemas. ¡Sigue así!";
+    points = points + 1
+  }
+
+  points = parseFloat(points);
+
+
+  if (points <= 0) {
+    document.getElementById("result_points").innerHTML = "Aún hay mucho que puedes mejorar, controlar tus finanzas te impulsará a lograr tus objetivos financieros. ¡No te rindas, nosotros te ayudamos!";
+  }
+
+  if (points == 2) {
+    document.getElementById("result_points").innerHTML = "¡Felicidades has conseguido un puntaje superior al del 60% de la gente que toma esta evaluación!";
+  }
+
+  if (points == 3) {
+    document.getElementById("result_points").innerHTML = "¡Felicidades has conseguido un puntaje superior al del 60% de la gente que toma esta evaluación! Tal vez estás listo/a para empezar a invertir tus ahorros y no pierdan valor, debido a la inflación.";
   }
 
 }
